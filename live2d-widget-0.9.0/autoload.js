8203ef13a1811d2580fdbca6db99e4a0e43ae193
Andy Lee <135169413+ideal130778@users.noreply.github.com>
2024-11-21 18:04:35 +0800

    Add files via upload

--- live2d-widget-0.9.0/autoload.js
+++ live2d-widget-0.9.0/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/ideal130778/images-recognition-for-aigc-purpose@main/live2d-widget-0.9.0/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/ideal130778/images-recognition-for-aigc-purpose@master/live2d-widget-0.9.0/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,12 +33,11 @@ if (screen.width >= 768) {
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: "https://cdn.jsdelivr.net/gh/ideal130778/images-recognition-for-aigc-purpose@main/live2d-widget-0.9.0/waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
 }
-
 
